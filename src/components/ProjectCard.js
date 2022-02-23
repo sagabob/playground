c0db5cecba501825cedfd634ec8e575354b0c532
DESK-TDP-01\bobpham <bobpham.tdp@gmail.com>
2022-02-24 08:21:06 +1100

    work in progress

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,9 +1,30 @@
 import React from 'react'
 
-const ProjectCard = () => {
+const ProjectCard = (projectInfo) => {
   return (
-    <div className='project-card'>
-        <h4>Project 1</h4>
+    <div className='card project-card'>
+      <div className="project-card-section">
+        
+        <div className="section-content">
+          <div className="project-wrapper">
+            <div className="project-item-wrapper">
+              <div className="project-item">
+                <div className="project-icon">
+                  <a href={projectInfo.link} target="_blank" rel="noreferrer" ><img src={projectInfo.main_tool_icon} alt="" /></a>
+                </div>
+                <div className="meta">
+                  <span className="meta-title">{projectInfo.title}</span>
+                  <span >
+                    <span>{projectInfo.start_time}</span>
+                    <span>&#8213;</span>
+                    <span>{projectInfo.end_time}</span>
+                  </span>                  
+                </div>
+              </div>              
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/pages/Demo.js
+++ src/pages/Demo.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import ProjectCard from '../components/ProjectCard'
+import ProfileData from '../content/profile.json'
 
 const Demo = () => {
     return (
         <div className="app-wrapper">
             <div className="container app-content">
                 <h2>Playground</h2>
+                <div className="container mt-6">
+                    <div className="columns is-multiline">
+                        <div className="column is-one-quarter-fullhd is-one-quarter-widescreen is-one-third-desktop is-one-third-tablet is-mobile">
+                            {ProfileData.projects.map((item, index) => (
+                                <ProjectCard {...item} key={index} />
+                            ))}
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
