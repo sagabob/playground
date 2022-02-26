20f65acd3001798163277ba1080201a5a5e1f8e0
DESK-TDP-01\bobpham <bobpham.tdp@gmail.com>
2022-02-26 17:08:50 +1100

    create a card

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,27 +1,53 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSquareArrowUpRight, faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+
 
 const ProjectCard = (projectInfo) => {
   return (
     <div className='card project-card'>
       <div className="project-card-section">
-        
-        <div className="section-content">
-          <div className="project-wrapper">
-            <div className="project-item-wrapper">
-              <div className="project-item">
-                <div className="project-icon">
-                  <a href={projectInfo.link} target="_blank" rel="noreferrer" ><img src={projectInfo.main_tool_icon} alt="" /></a>
+        <div className="project-wrapper">
+          <div className="project-header-wrapper">
+            <div className="project-header">
+              <div className="project-icon">
+                <div className="h-avatar">
+                  <img src={projectInfo.main_tool_icon} className="avatar" alt="" />
+                  <img className="badge" src={projectInfo.status} alt="location" />
                 </div>
-                <div className="meta">
-                  <span className="meta-title">{projectInfo.title}</span>
-                  <span >
-                    <span>{projectInfo.start_time}</span>
-                    <span>&#8213;</span>
-                    <span>{projectInfo.end_time}</span>
-                  </span>                  
-                </div>
-              </div>              
+              </div>
+              <div className="meta">
+                <span className="meta-title">{projectInfo.title}</span>
+                <span >
+                  <span>{projectInfo.start_time}</span>
+                  <span>&#8213;</span>
+                  <span>{projectInfo.end_time}</span>
+                </span>
+              </div>
+              <div className="project-status">
+                <FontAwesomeIcon icon={faCheckCircle} />
+              </div>
             </div>
+            <div className="project-notes">
+              <span>{projectInfo.note}</span>
+            </div>
+            <div className="project-notes avatar-stack">
+              {projectInfo.tools.map((item, index) => (
+                <div key={index} className="h-avatar is-small">
+                  <a href={item.tool_link} className="avatar-link"><img className="avatar" src={item.tool_icon} alt="" /></a>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="project-image-area">
+            <img className="project-image" src={projectInfo.project_image} alt="location" />
+          </div>
+          <div className="project-description">
+            <span>{projectInfo.project_description}</span>
+          </div>
+          <div className="card-footer project-action">
+            <a href={projectInfo.source_link} className="card-footer-item" target="_blank" rel="noreferrer"><span>View Source</span><FontAwesomeIcon icon={faSquareArrowUpRight} /></a>
+            <a href={projectInfo.app_link} className="card-footer-item" target="_blank" rel="noreferrer"><span>View App</span><FontAwesomeIcon icon={faSquareArrowUpRight} /></a>
           </div>
         </div>
       </div>
--- src/pages/Demo.js
+++ src/pages/Demo.js
@@ -3,17 +3,38 @@ import ProjectCard from '../components/ProjectCard'
 import ProfileData from '../content/profile.json'
 
 const Demo = () => {
+
+    const numberOfGroups = 4
+    const groups = []
+    for (let i = 0; i < numberOfGroups; i++)
+        groups[i] = []
+
+    let totalLength = ProfileData.projects.length    
+    let times = Math.ceil(totalLength/numberOfGroups)
+    
+    for (let t = 0; t < times; t++) {
+        for (let m = 0; m < numberOfGroups; m++) {
+            let ele = numberOfGroups * t + m
+            if (ele < totalLength)
+                groups[m].push(ProfileData.projects[ele])
+        }
+    }
+
+
+     
     return (
         <div className="app-wrapper">
             <div className="container app-content">
                 <h2>Playground</h2>
                 <div className="container mt-6">
                     <div className="columns is-multiline">
-                        <div className="column is-one-quarter-fullhd is-one-quarter-widescreen is-one-third-desktop is-one-third-tablet is-mobile">
-                            {ProfileData.projects.map((item, index) => (
-                                <ProjectCard {...item} key={index} />
+                        {groups.map((group_item, index) => (
+                            <div className="column is-one-quarter-fullhd is-one-quarter-widescreen is-one-third-desktop is-one-third-tablet is-mobile" key ={index}>
+                            {group_item.map((item, subindex) => (
+                                <ProjectCard {...item} key={subindex} />
                             ))}
-                        </div>
+                            </div>
+                        ))}                       
                     </div>
                 </div>
             </div>
