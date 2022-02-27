c738c9ef5ad1cd61bb0c902a9ee147b67f4e9e5c
DESK-TDP-01\bobpham <bobpham.tdp@gmail.com>
2022-02-27 18:32:39 +1100

    work in progress

---
+++ src/components/ComppactProjectCard.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSquareArrowUpRight, faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import {successColor,warningColor} from '../utils/colors'
+
+const ComppactProjectCard = (projectInfo) => {
+    return (
+        <div className='card project-compact-card' >
+            <div className="project-card-section">
+                <div className="project-wrapper">
+                    <div className="project-image-area">
+                        <img className="project-image" src={projectInfo.project_image} alt="location" />
+                    </div>
+                    <div className="project-header-wrapper">
+                        <div className="project-header">
+                            <div className="project-icon">
+                                <div className="h-avatar">
+                                    <img src={projectInfo.main_tool_icon} className="avatar" alt="" />
+                                    <img className="badge" src={projectInfo.status} alt="location" />
+                                </div>
+                            </div>
+                            <div className="meta">
+                                <span className="meta-title">{projectInfo.title}</span>
+                                <span >
+                                    <span>{projectInfo.start_time}</span>
+                                    <span>&#8213;</span>
+                                    <span>{projectInfo.end_time}</span>
+                                </span>
+                            </div>
+                            <div className="project-status">
+                                <FontAwesomeIcon icon={faCheckCircle} color={projectInfo.isUpdated? successColor: warningColor }/>
+                            </div>
+                        </div>
+                        <div className="project-notes">
+                            <span>{projectInfo.note}</span>
+                        </div>
+                        <div className="project-notes avatar-stack">
+                            {projectInfo.tools.map((item, index) => (
+                                <div key={index} className="h-avatar is-small">
+                                    <a href={item.tool_link} className="avatar-link"><img className="avatar" src={item.tool_icon} alt="" /></a>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ComppactProjectCard
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,17 +1,31 @@
 import React from 'react'
+import ComppactProjectCard from '../components/ComppactProjectCard'
+import ProjectCard from '../components/ProjectCard'
+import ProfileData from '../content/profile.json'
 
 const Dashboard = () => {
+
   return (
-    <>
-      <div className="app-wrapper">
-        <div className="container app-content">
-          <h2>DASHBOARD</h2>
-          <div className="container mt-6">
+    <div className="app-wrapper">
+      <div className="container app-content">
+        <h2>Playground</h2>
+        <div className="container mt-6">
+          <div className="row is-flex is-flex-direction-row">
+            {ProfileData.projects.map((group_item, index) => (
+              index % 2 === 0 ? (
+                <div className="column is-one-quarter-fullhd is-one-quarter-widescreen is-one-third-desktop is-one-third-tablet is-mobile" key={index}>
+                  <ProjectCard {...group_item} key={index} />
+                </div>)
+                : (<div className="column is-one-quarter-fullhd is-one-quarter-widescreen is-one-third-desktop is-one-third-tablet is-mobile" key={index}>
+                  <ComppactProjectCard  {...group_item} key={index} />
+                </div>)
+            ))}
           </div>
         </div>
       </div>
-    </>
+    </div>
   )
 }
+
 
 export default Dashboard
